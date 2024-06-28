002da81b950fbfc59377b4f7ba71594885853924
Rosendo <grosendoh73@gmail.com>
2024-06-28 15:35:18 -0600

    Remove cors

--- src/bootstrap.nest.ts
+++ src/bootstrap.nest.ts
@@ -5,8 +5,6 @@ import type { NestExpressApplication } from '@nestjs/platform-express'
 import { ValidationPipe } from '@nestjs/common'
 import { AllExceptionsFilter } from '@middlewares/errors'
 
-const allowedOrigins = ['http://localhost:4321', 'https://money-minder-xi.vercel.app']
-
 export const bootstrap = async (): Promise<NestExpressApplication> => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
@@ -14,19 +12,6 @@ export const bootstrap = async (): Promise<NestExpressApplication> => {
   app.useGlobalPipes(new ValidationPipe())
 
   app.useGlobalFilters(new AllExceptionsFilter())
-
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    },
-    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET, POST, PUT, DELETE, PATCH',
-    credentials: true
-  })
 
   return app
 }
