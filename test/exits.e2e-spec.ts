f8daa520f400e14ab174d3bbb7650b091c5608ff
Rosendo <grosendoh73@gmail.com>
2024-06-29 20:24:16 -0600

    ✅ Add exits test and update entries test

--- test/exits.e2e-spec.ts
+++ test/exits.e2e-spec.ts
@@ -5,7 +5,7 @@ import { AppModule } from '@/app.module'
 
 const userEmail = process.env.TEST_EMAIL
 
-describe('EntryController (e2e)', () => {
+describe('ExitController (e2e)', () => {
   let app: INestApplication
 
   beforeAll(async () => {
@@ -23,20 +23,20 @@ describe('EntryController (e2e)', () => {
 
   beforeEach(async () => {
     await app.get('DATABASE_CLIENT').execute({
-      sql: 'DELETE FROM money_entries WHERE user_email = ?',
+      sql: 'DELETE FROM money_exits WHERE user_email = ?',
       args: [userEmail]
     })
   })
 
   afterEach(async () => {
     await app.get('DATABASE_CLIENT').execute({
-      sql: 'DELETE FROM money_entries WHERE user_email = ?',
+      sql: 'DELETE FROM money_exits WHERE user_email = ?',
       args: [userEmail]
     })
   })
 
-  it('/api/entries/new-entry (POST)', async () => {
-    const newEntryDto = {
+  it('/api/exits/new-exit (POST)', async () => {
+    const newExitDto = {
       email: userEmail,
       fullName: 'Luis Rog',
       date: '2024-06-29',
@@ -45,35 +45,35 @@ describe('EntryController (e2e)', () => {
       description: 'Test Description'
     }
 
-    await request(app.getHttpServer()).post('/api/entries/new-entry').send(newEntryDto).expect(201)
+    await request(app.getHttpServer()).post('/api/exits/new-exit').send(newExitDto).expect(201)
   })
 
-  it('/api/entries/get-entries (GET)', async () => {
+  it('/api/exits/get-exits (GET)', async () => {
     const email = userEmail
 
     const response = await request(app.getHttpServer())
-      .get('/api/entries/get-entries')
+      .get('/api/exits/get-exits')
       .query({ email })
       .expect(200)
 
     expect(response.body).toBeInstanceOf(Array)
     if (response.body.length > 0) {
-      expect(response.body[0]).toHaveProperty('amount')
+      expect(response.body[0].email).toHaveProperty('amount')
       expect(response.body[0]).toHaveProperty('description')
       expect(response.body[0]).toHaveProperty('category')
       expect(response.body[0]).toHaveProperty('date')
     }
   })
 
-  it('/api/entries/get-entries-by-category-monthly (GET)', async () => {
+  it('/api/exits/get-exits-by-category-monthly (GET)', async () => {
     const query = {
       email: userEmail,
-      month: '06',
-      year: '2024'
+      month: 6,
+      year: 2024
     }
 
     const response = await request(app.getHttpServer())
-      .get('/api/entries/get-entries-by-category-monthly')
+      .get('/api/exits/get-exits-by-category-monthly')
       .query(query)
       .expect(200)
 
