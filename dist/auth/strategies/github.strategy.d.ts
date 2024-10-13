77821a719f52afb16e477b456934c1370ddc6bcd
Rosendo <grosendoh73@gmail.com>
2024-10-12 21:21:54 -0600

    ⬆️ Upgrade dependencies

--- dist/auth/strategies/github.strategy.d.ts
+++ dist/auth/strategies/github.strategy.d.ts
@@ -1,5 +1,5 @@
 import { Profile, Strategy } from 'passport-github2';
-declare const GitHubStrategy_base: new (...args: any[]) => InstanceType<typeof Strategy>;
+declare const GitHubStrategy_base: new (...args: any[]) => Strategy;
 export declare class GitHubStrategy extends GitHubStrategy_base {
     constructor();
     validate(_accessToken: string, _refreshToken: string, profile: Profile, _done: () => void): Promise<any>;
